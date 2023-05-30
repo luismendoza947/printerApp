66ef81eb95bc68973e97feb99429d4d0c3a51178
Luis Mendoza <luis.mendoza@neta.mx>
2023-05-30 11:07:07 -0600

    update qr code

--- App.tsx
+++ App.tsx
@@ -23,6 +23,39 @@ ThermalPrinterModule.defaultConfig = {
   timeout: 30000,
 };
 
+const text =
+  '[C]<img>https://via.placeholder.com/300.jpg</img>\n' +
+  '[L]\n' +
+  "[C]<u><font size='big'>ORDER N°045</font></u>\n" +
+  '[L]\n' +
+  '[C]================================\n' +
+  '[L]\n' +
+  '[L]<b>BEAUTIFUL SHIRT</b>[R]9.99e\n' +
+  '[L]  + Size : S\n' +
+  '[L]\n' +
+  '[L]<b>AWESOME HAT</b>[R]24.99e\n' +
+  '[L]  + Size : 57/58\n' +
+  '[L]\n' +
+  '[C]--------------------------------\n' +
+  '[R]TOTAL PRICE :[R]34.98e\n' +
+  '[R]TAX :[R]4.23e\n' +
+  '[L]\n' +
+  '[C]================================\n' +
+  '[L]\n' +
+  "[L]<font size='tall'>Customer :</font>\n" +
+  '[L]Raymond DUPONT\n' +
+  '[L]5 rue des girafes\n' +
+  '[L]31547 PERPETES\n' +
+  '[L]Tel : +33801201456\n' +
+  '[L]\n' +
+  "[C]<barcode type='ean13' height='10'>831254784551</barcode>\n" +
+  "[C]<qrcode size='20'>http://www.developpeur-web.dantsu.com/</qrcode>\n" +
+  '[L]\n' +
+  '[L]\n' +
+  '[L]\n' +
+  '[L]\n' +
+  '[L]\n';
+
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const scrollRef = React.useRef(null)
@@ -36,7 +69,7 @@ function App(): JSX.Element {
     try {
       setErrorMessage('')
       await ThermalPrinterModule.printBluetooth({
-        payload: "[C]<qrcode size='20'>http://www.pavemint.com/</qrcode>",
+        payload: text,
         printerNbrCharactersPerLine: 38,
       });
       console.log('PRINT>>>')
